e70d6e4403673fcef3e9cf40fe89c8618986a2e1
RegexFlux <57199180+RegexFlux@users.noreply.github.com>
2022-06-27 20:37:22 +0200

    [Button] BuyButtonComponent | Deck Icon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './pages/landing/landing.component';
+import { BuyButtonComponent } from './components/buy-button/buy-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    BuyButtonComponent
   ],
   imports: [
     BrowserModule,
