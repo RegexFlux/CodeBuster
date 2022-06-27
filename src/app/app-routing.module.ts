7272cd2e8cc9e73174867b61bc4fd1923dbfecd1
RegexFlux <57199180+RegexFlux@users.noreply.github.com>
2022-06-27 19:11:38 +0200

    [Landing] LandingComponent | Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingComponent} from "./pages/landing/landing.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LandingComponent
+  },
+  {
+    path: '**', redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
