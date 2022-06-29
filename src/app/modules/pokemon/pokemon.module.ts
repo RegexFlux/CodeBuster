d9b9cbb2ee0211b338211b3e4bd666e018ee6a92
RegexFlux <57199180+RegexFlux@users.noreply.github.com>
2022-06-29 19:01:05 +0200

    [Pokemon] PokemonModule & Component Updated

--- src/app/modules/pokemon/pokemon.module.ts
+++ src/app/modules/pokemon/pokemon.module.ts
@@ -3,17 +3,27 @@ import { CommonModule } from '@angular/common';
 import {PokemonComponent} from "../../pages/pokemon/pokemon.component";
 import {PokemonRoutingModule} from "./pokemon-routing.module";
 import {SearchModule} from "../search/search.module";
+import {MaterialModule} from "../material/material.module";
+import {MatIconModule} from "@angular/material/icon";
+import {IconModule} from "../icon/icon.module";
+import {NavigationButtonComponent} from "../../components/buttons/navigation-button/navigation-button.component";
+import {TranslateModule} from "@ngx-translate/core";
+import {TranslationModule} from "../translation/translation.module";
 
 
 
 @NgModule({
   declarations: [
-    PokemonComponent
+    PokemonComponent,
+    NavigationButtonComponent
   ],
   imports: [
     CommonModule,
     PokemonRoutingModule,
-    SearchModule
+    SearchModule,
+    IconModule,
+    MaterialModule,
+    TranslationModule
   ]
 })
 export class PokemonModule { }
--- src/app/pages/pokemon/pokemon.component.ts
+++ src/app/pages/pokemon/pokemon.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NavigationButton} from "../../interfaces/button";
 
 @Component({
   selector: 'app-pokemon',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pokemon.component.scss']
 })
 export class PokemonComponent implements OnInit {
+
+  navigationConfig: NavigationButton = {
+    routerLink: '',
+    text: 'home'
+  }
 
   constructor() { }
 
