45214bbde3e3fc712c4d7e11ebe7dc68873e1f8d
RegexFlux <57199180+RegexFlux@users.noreply.github.com>
2022-06-29 15:53:04 +0200

    [Pokemon] PokemonInterface

---
+++ src/app/interfaces/pokemon.ts
@@ -0,0 +1,25 @@
+export interface PokemonSearchResult {
+  pokemon: Pokemon[]
+}
+
+export interface Pokemon {
+  id: number;
+  name: string;
+  pokemon_v2_pokemonstats: PokemonStat[];
+  pokemon_v2_pokemontypes: PokemonType[];
+}
+
+export interface PokemonStat {
+  base_stat: number;
+  effort: number;
+  pokemon_v2_stat: { name: string }
+}
+
+export interface PokemonType {
+  pokemon_v2_type: { name: string }
+}
+
+export interface PokemonTypeAsset {
+  assetName: string;
+  color: string;
+}
