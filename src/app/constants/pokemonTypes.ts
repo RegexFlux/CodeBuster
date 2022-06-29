c020dbf54957c2ca6adfd2ccdad3367f8e0f53f8
RegexFlux <57199180+RegexFlux@users.noreply.github.com>
2022-06-29 15:57:28 +0200

    [Pokemon] PokemonTypesAssets

---
+++ src/app/constants/pokemonTypes.ts
@@ -0,0 +1,78 @@
+import {PokemonTypeAsset} from "../interfaces/pokemon";
+
+export const assetPath = '/assets/'
+
+export const pokemonTypesAssets: { [v: string]: PokemonTypeAsset } = {
+  normal: {
+    assetName: 'normal.svg',
+    color: '#a8a878'
+  },
+  fire: {
+    assetName: 'normal.svg',
+    color: '#f08030'
+  },
+  water: {
+    assetName: 'normal.svg',
+    color: '#6890f0'
+  },
+  grass: {
+    assetName: 'grass.svg',
+    color: '#78c850'
+  },
+  electric: {
+    assetName: 'electric.svg',
+    color: '#f8d030'
+  },
+  ice: {
+    assetName: 'ice.svg',
+    color: '#f8d030'
+  },
+  fighting: {
+    assetName: 'fighting.svg',
+    color: '#c03028'
+  },
+  poison: {
+    assetName: 'poison.svg',
+    color: '#a040a0'
+  },
+  ground: {
+    assetName: 'ground.svg',
+    color: '#e0c068'
+  },
+  flying: {
+    assetName: 'flying.svg',
+    color: '#a890f0'
+  },
+  psychic: {
+    assetName: 'psychic.svg',
+    color: '#f85888'
+  },
+  bug: {
+    assetName: 'bug.svg',
+    color: '#a8b820'
+  },
+  rock: {
+    assetName: 'rock.svg',
+    color: '#b8a038'
+  },
+  ghost: {
+    assetName: 'ghost.svg',
+    color: '#705898'
+  },
+  dark: {
+    assetName: 'dark.svg',
+    color: '#705848'
+  },
+  dragon: {
+    assetName: 'dragon.svg',
+    color: '#7038f8'
+  },
+  steel: {
+    assetName: 'steel.svg',
+    color: '#b8b8d0'
+  },
+  fairy: {
+    assetName: 'fairy.svg',
+    color: '#f0b6bc'
+  },
+}
