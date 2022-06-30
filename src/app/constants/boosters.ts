5aa0d22639d422a1db7e095e13ffaf2af68a15c2
RegexFlux <57199180+RegexFlux@users.noreply.github.com>
2022-06-30 15:13:01 +0200

    [Constant] Boosters Constants

---
+++ src/app/constants/boosters.ts
@@ -0,0 +1,71 @@
+import {SelectInterface, SelectOptionInterface, TextInterface} from "../interfaces/input";
+
+export const boosterInfos = [
+  {
+    name: 'redAndGreen',
+    readableName: 'Red & Green',
+    generation: 'Gen 1',
+    boosterAsset: 'assets/boosters/Gen1/red-green.png',
+    price: 50
+  },
+  {
+    name: 'blue',
+    readableName: 'Blue',
+    generation: 'Gen 1',
+    boosterAsset: 'assets/boosters/Gen1/blue.png',
+    price: 50
+  },
+  {
+    name: 'redAndBlue',
+    readableName: 'Red & Blue',
+    generation: 'Gen 2',
+    boosterAsset: 'assets/boosters/Gen2/red-blue.png',
+    price: 50
+  },
+  {
+    name: 'goldAndSilver',
+    readableName: 'Gold & Silver',
+    generation: 'Gen 2',
+    boosterAsset: 'assets/boosters/Gen2/gold-silver.png',
+    price: 50
+  },
+  {
+    name: 'crystal',
+    readableName: 'Crystal',
+    generation: 'Gen 2',
+    boosterAsset: 'assets/boosters/Gen2/crystal.png',
+    price: 50
+  },
+  {
+    name: 'rubyAndSapphire',
+    readableName: 'Ruby & Sapphire',
+    generation: 'Gen 3',
+    boosterAsset: 'assets/boosters/Gen3/ruby-sapphire.png',
+    price: 50
+  },
+]
+
+export const boostersSelect: SelectInterface = {
+  label: "Booster Edition",
+  options: boosterInfos.map((info): SelectOptionInterface => {
+    return {
+      value: info.name,
+      optionPrimaryInfo: info.generation,
+      optionSecondaryInfo: info.readableName
+    }
+  })
+};
+
+export const quantityInput: TextInterface = {
+  label: 'Order Quantity',
+  placeholder: 'Quantity',
+  prefix: {
+    isIcon: true,
+    element: 'payments'
+  },
+  suffix: {
+    isIcon: false,
+    element: '50 $'
+  },
+  textType: 'number'
+}
